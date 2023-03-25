577080db0c05a3afaefab2212fff5c096739708c
Mayomiolamide <akintayomayomiolamide@gmail.com>
2023-03-24 19:20:43 -0700

    configuration

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,10 @@
-# This Puppet manifest creates a file in /tmp
+#Create a file in /tmp
 
-file { 'holberton':
-  ensure  => file,
-  path    => '/tmp/holberton',
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet'
+file { 'school':
+ensure  => 'present',
+path    => '/tmp/school',
+mode    => '0744',
+owner   => 'www-data',
+group   => 'www-data',
+content => 'I love Puppet'
 }
